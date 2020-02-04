99dacb1803989cc67f00871d972dd6daaa218c19
JonahSimon <Jman19922@gmail.com>
2020-02-04 14:30:41 -0700

    Moving the cat around and vars

--- docs/js/cat.js
+++ docs/js/cat.js
@@ -1,3 +1,6 @@
+let catX = 0;
+let caty = 0;
+
 function seeCat() {
     let cat = document.getElementById("cat")
     cat.style= "display: block"
@@ -8,11 +11,21 @@ function hideCat() {
     cat.style= "display: none"
 }
 
+function moveCat() {
+    let cat = document.getElementById("cat")
+    catX = Math.random()*400;
+    catY = Math.random()*400;
+    cat.style = "left: " + catX + "px; top: " + catY + "px;"
+}
+
 function changeParagraph() {
     let para = document.getElementById("test")
     para.textContent = "Now I have changed this text"
 }
 
+function 
+
 hideCat()
 setTimeout(seeCat, 5000)
 setTimeout(changeParagraph, 10000)
+setInterval(moveCat, 5000)
