44a931cc07cea59a972b7ce9fd4b365da72f9572
JonahSimon <Jman19922@gmail.com>
2020-04-17 19:25:19 -0600

    adding a moving block for the basis of a top down game

--- docs/project/canvasjs/game1.js
+++ docs/project/canvasjs/game1.js
@@ -1,93 +1,59 @@
-var canvas = document.querySelector('canvas');
+var myGamePiece;
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+function startGame() {
+    c.start();
+    myGamePiece = new component(300, 300, "red", 100, 120);
+}
 
-var c = canvas.getContext('2d');
-
-var myGamePiece = new component(30, 30, "red", 100, 120);
-var isMoving;
+var c = {
+    canvas : document.querySelector('canvas'),
+    start : function() {
+      this.canvas.width = window.innerWidth;
+      this.canvas.height = window.innerHeight;
+      this.context = this.canvas.getContext("2d");
+      document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+      this.interval = setInterval(updateGameArea, 20);
+      window.addEventListener('keydown', function (e) {
+        c.key = e.keyCode;
+      })
+    
+      window.addEventListener('keyup', function (e) {
+        c.key = false;
+      })
+    },
+    clear : function() {
+      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+  }
 
 // class to add shapes in specific places to the canvas
-class component {
-    constructor(width, height, color, x, y) {
+function component(width, height, color, x, y) {
+        this.c = c;
         this.width = width;
         this.height = height;
         this.speedX = 0;
         this.speedY = 0;
         this.x = x;
         this.y = y;
-
-
         this.update = function() {
-            c.fillStyle = color;
-            c.fillRect(this.x, this.y, this.width, this.height);
+            ctx = c.context;
+            ctx.fillStyle = color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
         }
         this.newPos = function() {
             this.x += this.speedX;
             this.y += this.speedY;
         }  
-    }
 }
 
 function updateGameArea() {
+    c.clear();
+    myGamePiece.speedX = 0;
+    myGamePiece.speedY = 0;
+    if (c.key && c.key == 65) {myGamePiece.speedX = -1; } // move left
+    if (c.key && c.key == 68) {myGamePiece.speedX = 1; } // move right
+    if (c.key && c.key == 87) {myGamePiece.speedY = -1; } // move up
+    if (c.key && c.key == 83) {myGamePiece.speedY = 1; } // move down
     myGamePiece.newPos();
     myGamePiece.update();
-  }
-
-  
-// Functions surronding moving //  
-function moveup() {
-    myGamePiece.speedY -= 1;
-}
-  
-function movedown() {
-    myGamePiece.speedY += 1;
-}
-  
-function moveleft() {
-    myGamePiece.speedX -= 1;
-}
-  
-function moveright() {
-    myGamePiece.speedX += 1;
-}
-
-
-
-function keyDownHandler(event)
-{
-	var keyPressed = String.fromCharCode(event.keyCode);
-
-	if (keyPressed == "W")
-	{	
-        moveup();	
-		isMoving = true;
-	}
-	else if (keyPressed == "D")
-	{	
-        moveright();
-		isMoving = true;		
-	}
-	else if (keyPressed == "S")
-	{	
-        movedown();
-		isMoving = true;		
-	}
-	else if (keyPressed == "A")
-	{	
-        moveleft();
-		isMoving = true;		
-	}
-}
-
-function keyUpHandler(event)
-{
-	var keyPressed = String.fromCharCode(event.keyCode);
-	
-	if ((keyPressed == "W") || (keyPressed == "A") || 
-		(keyPressed == "S") || (keyPressed == "D"))
-	{
-		isMoving = false;
-	}
-}
+} 
